227a579fba90e49a7df71f8b35adfca7716fcae3
Subham Ghosh <74487802+subhamgcon@users.noreply.github.com>
2023-02-10 01:10:31 +0530

    Update signedMessage.js

--- public-key/signedMessage.js
+++ public-key/signedMessage.js
@@ -1,3 +1,21 @@
+// Signing the Hash
+// It's time to sign a message using our private key to prove our intention!
+
+// When signing a message with secp256k1 we can return the signature along with the recovery bit, allowing us to recover the public key from the signature. This will allow a blockchain node to take a signature of a transaction and understand which address authenticated this particular transaction.
+
+// 💭 This is an important point, a blockchain transaction not only indicates the intent of the person who signed it, it also authenticates them through public key cryptography! We'll see this more clearly when start signing transactions in future lessons.
+
+// 🏁 Your Goal: Sign Message
+// Let's sign a message!
+
+// First step is to hash it using the hashMessage function you created in the last stage (we've already imported it for you on line 2)
+// Once you have the message hash, use the sign method from ethereum-cryptography.
+// Note
+// The sign method will take your message hash along with the constant PRIVATE_KEY declared at the top of the file. This private key is a valid key that could be used to authorize blockchain transactions. Never use this specific key because it is published on the internet, so many people including yourself could authenticate with this specific private key. In future lessons we'll be discussing good private key hygene to avoid losing funds.
+// The ethereum-cryptography library uses noble-secp256k1, so the import comes from ethereum-cryptography but the detailed documentation is in the readme of noble-secp256k1.
+// The sign method takes an optional third parameter called options, which you'll see in the documentation. Use this parameter to return the recovered bit so that the public key can be recovered from this signature.
+
+
 const secp = require("ethereum-cryptography/secp256k1");
 const hashMessage = require('./hashMessage');
 
